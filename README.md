0a1c4cf2f2790720e1e280810960d7555a770a73
Md Sabbir Ahmed <mdsabbirahmed@Mds-MacBook-Air.local>
2022-11-28 17:22:48 +0600

    added

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-# 
+# কেনাবেচা ডট কম 
+# Live Link : 
+
+# Details
+* A simple Ecommerce website where you can buy mobile
+* You can add to card you product
+* You can wishlisted your desired product to buy late
+* In this website, 3 category can logged in. 1. User/Buyer 2. Selller  3. Admin
+* An Admin can remove any user and seller
+* An Admin can see the reported items
+
 
 <!-- Admin
 email: a1a@aa.com
--- src/Firebase/Firebase.init.js
+++ src/Firebase/Firebase.init.js
@@ -5,12 +5,20 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDd18qq_rnE89yfLdlP7Qm05fG6ltMDBW0",
-  authDomain: "resale-market-d3fe2.firebaseapp.com",
-  projectId: "resale-market-d3fe2",
-  storageBucket: "resale-market-d3fe2.appspot.com",
-  messagingSenderId: "832219195401",
-  appId: "1:832219195401:web:9aac1e66d4c2ba1d7c70d8"
+  // apiKey: "AIzaSyDd18qq_rnE89yfLdlP7Qm05fG6ltMDBW0",
+  // authDomain: "resale-market-d3fe2.firebaseapp.com",
+  // projectId: "resale-market-d3fe2",
+  // storageBucket: "resale-market-d3fe2.appspot.com",
+  // messagingSenderId: "832219195401",
+  // appId: "1:832219195401:web:9aac1e66d4c2ba1d7c70d8"
+
+
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
